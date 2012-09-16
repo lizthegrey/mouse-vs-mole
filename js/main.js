98cfac1c04f858c63b3caab34d335887a12ba01e
Liz Fong-Jones <lizfong@mit.edu>
2012-09-16 12:59:22 -0400

    stylistic and semantic fixes

--- js/main.js
+++ js/main.js
@@ -1,101 +1,126 @@
-var PLAYGROUND_HEIGHT = 600;
-var PLAYGROUND_WIDTH = 800;
+var BLOCK_SIZE = 20;
+var PLAYER_SIZE = 15;
+var NUM_COLORS = 4;
+
+var GRID_WIDTH = 40;
+var GRID_HEIGHT = 30;
+
+var PLAYGROUND_WIDTH = BLOCK_SIZE * GRID_WIDTH;
+var PLAYGROUND_HEIGHT = BLOCK_SIZE * GRID_HEIGHT;
+
+// Where is the mystery +5 coming from? -lizf
+var START_XCOORD_P1 = 2;
+var START_XPOS_P1 = START_XCOORD_P1 * BLOCK_SIZE;
+var START_XCOORD_P2 = 37;
+var START_XPOS_P2 = START_XCOORD_P2 * BLOCK_SIZE + 5;
+var START_YCOORD = 14;
+var START_YPOS = BLOCK_SIZE * START_YCOORD + 5;
 
 function buildPlayground() {
-    $('#game').playground({height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH, keyTracker: true});
+  $('#game').playground({
+      height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH,
+      keyTracker: true});
 
-    $.playground().addGroup("background", {height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
-    $.playground().addGroup("actors", {height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
+  $.playground().addGroup('background', {
+      height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
+  $.playground().addGroup('actors', {
+      height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
 }
 
 function addBackground() {
-    var background1 = new $.gameQuery.Animation({imageURL: "sprites/800x600.png"});
-    $('#background').addSprite("background1", {animation: background1, height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
+  var background1 = new $.gameQuery.Animation({
+      imageURL: 'sprites/800x600.png'});
+  $('#background').addSprite('background1', {
+      animation: background1,
+      height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
 }
 
 function addActors() {
-	$('#actors').addGroup("player1",{posx: 40,posy:285,width:15,height:15});
-	$('#actors').addGroup("player2",{posx: 745,posy:285,width:15,height:15});
-	var player1 = new player($("#player1"),1,40,285);
-	var player2 = new player($("#player2"),2,745,285);
-	$('#player1')
-		.addSprite("player1spr", {animation: player1.player, height: 15, width: 15, posx: 0, posy: 0});
-	$('#player2')
-		.addSprite("player2spr", {animation: player2.player, height: 15, width: 15, posx: 0, posy: 0});
-	$('#player1')[0].player = player1;
-	$('#player2')[0].player = player2;
-	
-	var block1 = new $.gameQuery.Animation({imageURL: "sprites/Block1.png"});
-	var block2 = new $.gameQuery.Animation({imageURL: "sprites/Block2.png"});
-	var block3 = new $.gameQuery.Animation({imageURL: "sprites/Block3.png"});
-	var block4 = new $.gameQuery.Animation({imageURL: "sprites/Block4.png"});
-	
-	var rand = 0;
-	var thisBlock = block1;
-	for(var i = 0; i < 40; i++) {
-		for(var j = 0; j < 30; j++) {
-			if (j == 14 && (i == 2 || i ==  37)) continue;
-			rand = Math.floor(Math.random()*4);
-			switch(rand) {
-				case 0:
-					thisBlock = block1;
-					break;
-				case 1:
-					thisBlock = block2;
-					break;
-				case 2:
-					thisBlock = block3;
-					break;
-				default:
-					thisBlock = block4;
-					break;
-			}
-			$('#actors').addSprite("block"+i+','+j, {animation: thisBlock, height: 20, width: 20, posx: i*20, posy: j*20});
-		}
-	}
+  $('#actors').addGroup('player1', {
+      posx: START_XPOS_P1, posy: START_YPOS,
+      width: PLAYER_SIZE, height: PLAYER_SIZE});
+  $('#actors').addGroup('player2', {
+      posx: START_XPOS_P2, posy: START_YPOS,
+      width: PLAYER_SIZE, height: PLAYER_SIZE});
+  var player1 = new player($('#player1'), 1,
+                           START_XPOS_P1, START_YPOS);
+  var player2 = new player($('#player2'), 2,
+                           START_XPOS_P2, START_YPOS);
+  $('#player1').addSprite('player1spr', {
+      animation: player1.player,
+      height: PLAYER_SIZE, width: PLAYER_SIZE,
+      posx: 0, posy: 0});
+  $('#player2').addSprite('player2spr', {
+      animation: player2.player,
+      height: PLAYER_SIZE, width: PLAYER_SIZE,
+      posx: 0, posy: 0});
+  $('#player1')[0].player = player1;
+  $('#player2')[0].player = player2;
+
+  var block_sprites = [];
+  for (var i = 0; i < NUM_COLORS; i++) {
+    block_sprites[i] = new $.gameQuery.Animation({
+        imageURL: 'sprites/Block' + (i + 1) + '.png'});
+  }
+
+  var rand = 0;
+  var thisBlock = block_sprites[0];
+  for (var x = 0; x < GRID_WIDTH; x++) {
+    for (var y = 0; y < GRID_HEIGHT; y++) {
+      if (y == START_YCOORD &&
+          (x == START_XCOORD_P1 || x == START_XCOORD_P2)) {
+        // Player start position
+        continue;
+      }
+      rand = Math.floor(Math.random() * 4);
+      thisBlock = block_sprites[rand];
+      $('#actors').addSprite('block' + x + ',' + y, {
+          animation: thisBlock,
+          height: BLOCK_SIZE, width: BLOCK_SIZE,
+          posx: x * BLOCK_SIZE, posy: y * BLOCK_SIZE});
+    }
+  }
 }
 
-function player(node,playerNum,xpos,ypos){
-	this.node = node;
-	this.playerNum = playerNum;
-	this.yVel = 0;
-	switch(this.playerNum) {
-		case 1:
-			this.player = new $.gameQuery.Animation({imageURL: "sprites/Player1.png"});
-			break;
-		case 2:
-			this.player = new $.gameQuery.Animation({imageURL: "sprites/Player2.png"});
-			break;
-	}
-	return true;
+function player(node, playerNum, xpos, ypos) {
+  this.node = node;
+  this.playerNum = playerNum;
+  this.yVel = 0;
+  this.player = new $.gameQuery.Animation({
+      imageURL: 'sprites/Player' + this.playerNum + '.png'});
+  return true;
 }
 
 function addFunctionality() {
-	$.playground().registerCallback(function(){
-		playerMove(1);
-		playerMove(2); },30)
+  $.playground().registerCallback(function() {
+        playerMove(1);
+        playerMove(2);},
+      30);
 }
 
 function playerMove(player) {
-	if(jQuery.gameQuery.keyTracker[65] && player == 1 || jQuery.gameQuery.keyTracker[37] && player == 2){ //this is left!
-		var nextpos = parseInt($("#player"+player).x())-2;
-		if(nextpos > 0){
-			$("#player"+player).x(nextpos);
-		}
-	}
-	if(jQuery.gameQuery.keyTracker[68] && player == 1 || jQuery.gameQuery.keyTracker[39] && player == 2){ //this is right! (d)
-		var nextpos = parseInt($("#player"+player).x())+2;
-		if(nextpos < PLAYGROUND_WIDTH - 20){
-			$("#player"+player).x(nextpos);
-		}
-	}
+  if (jQuery.gameQuery.keyTracker[65] && player == 1 ||
+      jQuery.gameQuery.keyTracker[37] && player == 2) {
+    // this is left!
+    var nextpos = parseInt($('#player' + player).x()) - 2;
+    if (nextpos > 0) {
+      $('#player' + player).x(nextpos);
+    }
+  }
+  if (jQuery.gameQuery.keyTracker[68] && player == 1 ||
+      jQuery.gameQuery.keyTracker[39] && player == 2) {
+    //this is right! (d)
+    var nextpos = parseInt($('#player' + player).x()) + 2;
+    if (nextpos < PLAYGROUND_WIDTH - BLOCK_SIZE) {
+      $('#player' + player).x(nextpos);
+    }
+  }
 }
 
 $(document).ready(function() {
-    buildPlayground();
-    addBackground();
-    addActors();
-    addFunctionality();
-    
-    $.playground().startGame();
+  buildPlayground();
+  addBackground();
+  addActors();
+  addFunctionality();
+  $.playground().startGame();
 });
