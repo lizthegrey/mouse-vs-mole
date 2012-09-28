1b2f24c3676e474555c96a7694878b74b03c5abf
ant1jr <ant1jr@mit.edu>
2012-09-27 23:29:01 -0400

    Add info to page and stop arrow key scroll

--- js/main.js
+++ js/main.js
@@ -582,6 +582,19 @@ function deathFromBelow() {
   should_creep = false;
 }
 
+var ar=new Array(33,34,35,36,37,38,39,40);
+
+$(document).keydown(function(e) {
+     var key = e.which;
+      //console.log(key);
+      //if(key==35 || key == 36 || key == 37 || key == 39)
+      if($.inArray(key,ar) > -1) {
+          e.preventDefault();
+          return false;
+      }
+      return true;
+});
+
 $(document).ready(function() {
   buildPlayground();
   addBackground();
